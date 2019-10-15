bdb703fca5738badfa460972b17e2e5556c3d0a0
文宇 <xu3jp6m369@gmail.com>
2019-10-15 16:11:48 +0700

    temp.py

---
+++ temp.py
@@ -0,0 +1,20 @@
+
+'''
+def sum(*numbers):
+    total =0
+    for number in numbers:
+    
+         total+= number
+    
+    return total
+
+print(sum(1,12, 4, 8,15)) 
+'''   
+def sum(*numbers):
+    
+    total=0
+    for number in numbers:
+        total+= number
+    return total
+print(sum(1,2,3,4,5,6,7,8,9))    
+    

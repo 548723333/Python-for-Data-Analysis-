777aa246be076a4106f92ff06bb2c31263932db8
文宇 <xu3jp6m369@gmail.com>
2019-10-18 08:25:45 +0700

    practice 2.1.py

---
+++ practice 1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 21:24:54 2019
+
+@author: Hwayne
+
+Purpose:practice
+
+"""
+#logic error
+print("logic error")
+print("Fahrenheit to Celsius")
+
+print(5/9*35-32)
+
+print("currect")
+print(5/9*(35-32))
